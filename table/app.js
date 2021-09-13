f4f139f02f98b1bb592f949f12e4c593f99faa39
touraye <touraye07@gmail.com>
2021-09-13 12:53:50 +0000

    creating student-grade-book

---
+++ table/app.js
@@ -0,0 +1,34 @@
+const grade = [
+	{
+		id: 1,
+		name: 'ebrima faal',
+		marks: {
+			java: 80,
+			web: 90,
+			networking: 75,
+			pld: 99,
+		},
+	},
+	{
+		id: 2,
+		name: 'alasanna camara',
+		marks: {
+			java: 80,
+			web: 90,
+			networking: 75,
+			pld: 99,
+		},
+	},
+	{
+		id: 1,
+		name: 'ismail turner',
+		marks: {
+			java: 80,
+			web: 90,
+			networking: 75,
+			pld: 99,
+		},
+	},
+];
+
+console.log(grade);
